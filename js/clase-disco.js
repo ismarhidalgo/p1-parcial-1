54757d85fe5f931c13f541f8c6fc50b11268989b
Lele <nicolas.lcorbalan@davinci.edu.ar>
2024-10-21 03:56:54 -0300

    Nuevo JavaScript, más optimizado, más legible, y con más implementacion de métodos

--- js/clase-disco.js
+++ js/clase-disco.js
@@ -1,1 +1,54 @@
 'use strict';
+
+class Disco {
+    // Ordenadas en el orden en el que aparecen en el json (sin ningún motivo aparente)
+    #nombre;
+    #artista;
+    #id;
+    #portada;
+    #pistas;
+
+    constructor(nombre, artista, id, portada, _pistas){
+        this.#nombre = nombre;
+        this.#artista = artista;
+        this.#id= id;
+        this.#portada = portada;
+        this.#pistas = _pistas || [];
+    }
+
+    /*
+     * Metodo que retorna la estructura html del disco, junto con sus pistas
+     */
+    generarEstructuraHtml() {
+        return `
+            <article>
+                <header>
+                    <div>
+                        <h2>${this.#nombre}</h2>
+                        <p>${this.#artista} ● ${this.#pistas.length} Pistas, ${this.getDuracionTotal.toHhMmSs(2)}, (promedio: ${(this.getDuracionTotal / this.#pistas.length).toHhMmSs(2)}) </p>
+                        <p>${this.#id}</p>
+                    </div>
+                    <div>
+                        <img alt="Portada del disco: ${this.#nombre}" src="${this.#portada}">
+                    </div>
+                </header>
+                <div>
+                    <ul>
+                        ${this.#pistas.map(pista => pista.generarEstructuraHtml()).join()}
+                    </ul>
+                </div>
+            </article>
+        `;
+    }
+
+    /*
+     * Función que retorna la duración total del disco entre todas las pistas que tiene
+     */
+    get getDuracionTotal() {
+        return this.#pistas.reduce((duracionTotal, pista) => duracionTotal + pista.getDuracion, 0);
+    }
+
+    get getId() {
+        return this.#id;
+    }
+}
--- js/clase-pista.js
+++ js/clase-pista.js
@@ -1,1 +1,38 @@
 'use strict';
+
+class Pista {
+    #nombre;
+    #duracion;
+
+    constructor(nombre, duracion) {
+        this.#nombre = nombre;
+        this.#duracion = duracion;
+    }
+
+    /*
+     * Función que genera la estructura html de la pista
+     */
+    generarEstructuraHtml(_clasesExtra = "") {
+        return `
+            <li>
+                <h3>${this.#nombre}</h3>
+                <p class="${this.obtenerClases()}">${this.#duracion.toHhMmSs()}</p>
+            </li>
+        `;
+    }
+
+    /*
+     * Función que retorna un string con las clases que la
+     * pista debería tener en base a sus características
+     */
+    obtenerClases() {
+        let clases = "pistas__duracion";
+        if(this.#duracion > 180) clases += " --larga";
+
+        return clases;
+    }
+
+    get getDuracion() {
+        return this.#duracion;
+    }
+}
